4b051156eb2c70b8cfd9888d55115263b4ca6ecf
BryceVerberne <bryceverberne@gmail.com>
2023-04-05 14:49:56 -0700

    ✨ Add Pointer Types section

--- CProjects/PointerBasics/main.c
+++ CProjects/PointerBasics/main.c
@@ -14,7 +14,9 @@ int main() {
     // -------------------
     // Pointer Assignments
     // -------------------
-    printf("Pointer Assignments\n");
+    printf("-------------------");
+    printf("\nPointer Assignments");
+    printf("\n-------------------\n");
 
     int a = 10;
     int *p = &a;  // This is our pointer. Notice how there is a '*' before the variable name.
@@ -35,7 +37,7 @@ int main() {
     // Modify the value in 'a' with our pointer, 'p'.
     printf("\nOriginal value of 'a' is: %d\n", a);
 
-    *p = 12; // Now, 'a' should have a value of 12. This concept is called dereferencing.
+    *p = 12; // Now, 'a' should have a value of 12. This concept is called de-referencing.
 
     printf("Modified value of 'a' is: %d\n", a);
 
@@ -53,9 +55,11 @@ int main() {
 
 
     // -------------------
-    // Pointer Arithemtic
+    // Pointer Arithmetic
     // -------------------
+    printf("\n\n-------------------");
     printf("\nPointer Arithmetic");
+    printf("\n-------------------");
 
     // Reset our 'p' value
     a = 10;
@@ -68,7 +72,7 @@ int main() {
 
     // Add one to 'p'.
     // The address increments by 4 bytes, since this is the size of one word (four bytes or one integer variable).
-    printf("Adding 1 to 'p' gives us %d\n", (p+1));
+    printf("\nAdding 1 to 'p' gives us %d\n", (p+1));
 
     // Increment 'p' by 2
     // The address will increment by 8 bytes.
@@ -76,7 +80,64 @@ int main() {
 
     // Let's find the value at address p+1 (use a * before our pointer)
     // We get a random value since we have no value allocated to this memory address.
-    printf("Value at address 'p'+1 is %d\n", *(p+1));
+    printf("\nIncremented '*p' gives us %d\n", *(p+1));
+
+
+    // -------------------
+    // Pointer Types
+    // -------------------
+
+    // Info:
+    //  - Pointer variables are strongly typed. You need a pointer variable of a particular type to store a particular
+    //    type of variable. Example: int* -> int & char* -> char.
+    //  - Pointer variables don't only store addresses, but they are used to dereference these addresses to access
+    //    and modify the values in these addresses.
+
+    printf("\n\n-------------------");
+    printf("\nPointer Types");
+    printf("\n-------------------");
+
+    a = 1025;
+    p = &a;
+
+    // Here is list of the different sizes of data types
+    printf("\nData Type Sizes:\n");
+    printf("int - %d bytes\n", sizeof(int));
+    printf("char - %d bytes\n", sizeof(char));
+    printf("float - %d bytes\n", sizeof(float));
+    printf("double - %d bytes\n", sizeof(double));
+
+    // Our pointer currently holds the address of 'a' and points to its value.
+    printf("\nVariable 'p':\n");
+    printf("Address = %d\n", p);
+    printf("Value = %d\n", *p);
+
+    // Increment '*p' by one. Hint: Since nothing is allocated to that position in memory, we'll get a random number.
+    printf("\nIncrement 'p':\n");
+    printf("Address = %d\n", (p+1));
+    printf("Value = %d\n", *(p+1));
+
+    // Create a char pointer
+    char *p0;
+    // p0 = p; ('p0' holds the address of our first pointer, 'p'.)
+    // This doesn't work since 'p' is an integer pointer and 'p0' is a character pointer.
+
+    // We can type cast 'p' to character pointer in order for us to assign 'p0' to 'p'.
+    p0 = (char*)p;
+
+    // Notice how the address is the same as 'p'. However, why do we get 1 as our value?
+    // Since a char is one byte, we only take into consideration the first of four bytes of our 'a' integer variable.
+    // For 1025, for example, it is written as 1025 = 00000000 00000000 00000100 00000001.
+    // Here, we only consider 0000001, which is 1.
+    printf("\nVariable 'p0':\n");
+    printf("Address = %d\n", p0);
+    printf("Value = %d\n", *p0);
+
+    // Given that we received 00000001 for '*p0', incrementing by 1 in a char pointer should shift us to the next byte.
+    // Therefore, we should receive 00000100 or 4.
+    printf("\nIncrement '*p0':\n");
+    printf("Address = %d\n", (p0+1));
+    printf("Value = %d\n", *(p0+1));
 
     return 0;
 }
