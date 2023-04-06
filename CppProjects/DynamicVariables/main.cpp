d8c77783aedf83ccd0cd4c3e28bd31812c281f16
BryceVerberne <bryceverberne@gmail.com>
2023-04-05 17:28:39 -0700

    :arrows_counterclockwise: Allocate & deallocate dynamic variables

--- CppProjects/DynamicVariables/main.cpp
+++ CppProjects/DynamicVariables/main.cpp
@@ -21,20 +21,24 @@ int main()
     int *three = nullptr;
     int result;
 
-    //	Fill in code to allocate the integer variable one here
+    //	Allocate the integer variable one here
+    one = new int;
 
-    //	Fill in code to allocate the integer variable two here
+    //	Allocate the integer variable two here
+    two = new int;
 
-    //	Fill in code to allocate the integer variable three here
+    //	Allocate the integer variable three here
+    three = new int;
 
-    //	Fill in code to allocate the character array pointed to by name
+    //	Allocate the character array pointed to by name
+    name = new char[10];
 
     cout << "Enter your last name with exactly 10 characters." << endl;
     cout << "If your name has < 10 characters, repeat last letter. " << endl
          << "Blanks at the end do not count." << endl;
 
     for (pos = 0; pos < MAXNAME; pos++)
-        cin >>	// Fill in code to read a character into the name array
+        cin >> // Fill in code to read a character into the name array
             // WITHOUT USING a bracketed subscript
 
             cout << "Hi ";
@@ -58,7 +62,11 @@ int main()
 
             cout << "The sum of the three values is " << result << endl;
 
-    // Fill in code to deallocate one, two, three and name
+    // Deallocate one, two, three and name
+    delete one;
+    delete two;
+    delete three;
+    delete [] name;
 
     return 0;
 }
