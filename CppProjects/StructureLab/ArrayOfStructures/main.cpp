5060dc22ab65bfff2d03b44f9ed3d9a5eb56593b
BryceVerberne <bryceverberne@gmail.com>
2023-04-06 15:09:02 -0700

    ✨ Process & display user-filled array with calculation

--- CppProjects/StructureLab/ArrayOfStructures/main.cpp
+++ CppProjects/StructureLab/ArrayOfStructures/main.cpp
@@ -11,43 +11,49 @@
 #include <iomanip>
 using namespace std;
 
-// Fill in code to declare a structure called taxPayer that has three
-// members:	taxRate, income, and taxes — each of type float
+// The taxPayer structure holds data about an individual's tax-related information.
+struct taxPayer
+{
+    float taxRate;     // Tax rate for the taxpayer's income
+    float income;      // Taxpayer's total income
+    float taxes;       // Calculated taxes owed (taxRate * income)
+};
 
 int main()
 {
-    // Fill in code to define an array named citizen which holds
-    // 5 taxPayers structures
+    // Define an array named citizen, which holds 5 taxPayers structures.
+    taxPayer citizen[5];
 
     cout << fixed << showpoint << setprecision(2);
 
-    cout << "Please enter the annual income and tax rate for 5 tax payers: ";
-    cout << endl << endl << endl;
+    cout << "Enter the annual income and tax rate for 5 tax payers." << endl << endl;
 
-    for (int count = 0; count < 5; count++)
-    {
+    // Prompt the user to input tax info for all 5 citizens
+    for (int count = 0; count < 5; count++) {
         cout << "Enter this year's income for tax payer " << (count + 1);
         cout << ": ";
 
-        // Fill in code to read in the income to the appropriate place
+        // Read & store the income of the citizen
+        cin >> citizen[count].income;
 
-        cout << "Enter the tax rate for tax payer # " << (count + 1);
+        cout << "Enter the tax rate for tax payer " << (count + 1);
         cout << ": ";
 
-        // Fill in code to read in the tax rate to the appropriate place
+        // Read & store the tax rate of the citizen
+        cin >> citizen[count].taxRate;
 
-        // Fill in code to compute the taxes for the citizen and store it
-        // in the appropriate place
+        // Compute the taxes for the citizen & store the result
+        citizen[count].taxes = citizen[count].taxRate * citizen[count].income;
 
         cout << endl;
     }
 
-    cout << "Taxes due for this year: " << endl << endl;
+    cout << endl;
+    cout << "Taxes due for this year: " << endl;
 
-    // Fill in code for the first line of a loop that will output the
-    // tax information
-    {
-        cout << "Tax Payer # " << (index + 1) << ": " << "$ "
+    // Output tax info for all 5 citizens
+    for(int index = 0; index < 5; ++index) {
+        cout << "Tax Payer " << (index + 1) << ": " << "$"
              << citizen[index].taxes << endl;
     }
 
