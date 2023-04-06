92d3b7692f1e2d333f4eaa512e90cc24d43ed574
BryceVerberne <bryceverberne@gmail.com>
2023-04-06 11:15:22 -0700

    Add AirportStats lab

---
+++ CppProjects/AirportStats/main.cpp
@@ -0,0 +1,18 @@
+/*
+*  Title:  Airport Stats
+*  Desc:   This program uses structures to store monthly info for an airport.
+*  Author: Bryce Verberne
+*  Date:   04/06/2023
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!\n";
+
+    return 0;
+} 
