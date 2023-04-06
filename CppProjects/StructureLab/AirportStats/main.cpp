bfc21a798fb18ff0e00d4dff3f8835a4eae27de0
BryceVerberne <bryceverberne@gmail.com>
2023-04-06 16:22:47 -0700

    🔖 Break program into TODO sections

--- CppProjects/StructureLab/AirportStats/main.cpp
+++ CppProjects/StructureLab/AirportStats/main.cpp
@@ -10,9 +10,25 @@
 #include <iostream>
 using namespace std;
 
+// TODO: Create a structure to store the following:
+//        - Total number of planes landed
+//        - Total number of planes departed
+//        - Greatest number of planes that landed in a given day that month
+//        - Least number of planes that landed in a given day that month
+
 int main() {
 
-    cout << "Hello World!\n";
+    // TODO: Create an array of 12 structures to hold travel info for an entire year
+
+    // TODO: Prompt user to enter data for each month.
+
+    // TODO: Calculate & output the average monthly number of landing planes
+
+    // TODO: Calculate & output the average monthly number of departing planes
+
+    // TODO: Calculate & output the total number of departing & landing planes for the year
+
+    // TODO: Calculate & output greatest & least amount that landed on any one day & the month it occurred on
 
     return 0;
 } 
