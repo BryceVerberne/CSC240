829fcef8cba431069ed63c1a22d47b328c7144a2
BryceVerberne <bryceverberne@gmail.com>
2023-04-07 15:47:54 -0700

    Add FileHandling tutorial

---
+++ CProjects/FileHandling/main.c
@@ -0,0 +1,17 @@
+/*
+*  Title:  File Handling
+*  Desc:   This program demonstrates how to work with files in C.
+*  Author: Bryce Verberne
+*  Date:   04/07/2023
+*/
+
+
+
+#include <stdio.h>
+
+int main() {
+
+
+
+    return 0;
+}
