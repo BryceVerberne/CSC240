f72df5bfbecf2e6b89563655d58ec94a5f3a95bc
BryceVerberne <bryceverberne@gmail.com>
2023-04-07 19:22:33 -0700

    Add Triangles project

---
+++ CProjects/Triangles/main.c
@@ -0,0 +1,15 @@
+/*
+*  Title:  Triangles
+*  Desc:   This program demonstrates how to read from a file & operate on its information
+*  Author: Bryce Verberne
+*  Date:   04/07/2023
+*/
+
+
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
