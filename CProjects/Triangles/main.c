97fd981d65de80cac2fb7b6415076833f3e3ffe7
BryceVerberne <bryceverberne@gmail.com>
2023-04-07 21:05:13 -0700

    📂 Implement command-line file opening with error handling

--- CProjects/Triangles/main.c
+++ CProjects/Triangles/main.c
@@ -9,7 +9,35 @@
 
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    // To run the compiled program & provide the Triangle1.txt file, type the following:
+    //  > gcc -o Triangles
+    //  > ./Triangles Triangle1.txt
+
+    // Create FILE pointer
+    FILE *f = NULL;
+
+    // Determine if the user provided the file name
+    if (argc < 2) {
+        printf("> ./%s <filename>\n", argv[0]);
+
+        return 1;
+    }
+
+    // Open the file in read mode
+    f = fopen(argv[1], "r");
+
+    // Determine if the file has been opened successfully
+    if (f != NULL) {
+        printf("File Open Successful");
+    }
+    else {
+        printf("File Open Unsuccessful");
+    }
+
+    // Close the file
+    fclose(f);
 
     return 0;
 }
