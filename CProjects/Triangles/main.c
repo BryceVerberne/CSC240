cbf69225fbcec57a007b7bdb12a0c314b56c9ec4
BryceVerberne <bryceverberne@gmail.com>
2023-04-07 22:23:08 -0700

    ✨ Add loop to scan file & print only acceptable dimensions

--- CProjects/Triangles/main.c
+++ CProjects/Triangles/main.c
@@ -11,13 +11,17 @@
 
 int main(int argc, char *argv[]) {
 
-    // Create FILE pointer
+    // Declare & initialize FILE pointer
     FILE *f = NULL;
+
+    // Declare triangle dimension variables
+    int val1;
+    int val2;
+    int val3;
 
     // Determine if the user provided the file name
     if (argc < 2) {
         printf("Usage: %s <filename>\n", argv[0]);
-
         return 1;
     }
 
@@ -26,7 +30,24 @@ int main(int argc, char *argv[]) {
 
     // Determine if the file has been opened successfully
     if (f == NULL) {
-        printf("File %s cannot be opened.", argv[1]);
+        printf("File %s cannot be opened.\n", argv[1]);
+        return 1;
+    }
+
+    // Output acceptable triangle dimensions
+    //  - Criteria:
+    //     1. a + b > c
+    //     2. a + c > b
+    //     3. b + c > a
+    while (fscanf(f, "%d, %d, %d\n", &val1, &val2, &val3) == 3) {
+        if ((val1 + val2) > val3) {          // a + b > c
+            if ((val1 + val3) > val2) {      // a + c > b
+                if ((val2 + val3) > val1) {  // b + c > a
+                    // Print the triangle dimensions
+                    printf("%d, %d, %d\n", val1, val2, val3);
+                }
+            }
+        }
     }
 
     // Close the file
