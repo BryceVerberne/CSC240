2d975108ebd9d211d6c79f3ec1bf71415813bc77
BryceVerberne <bryceverberne@gmail.com>
2023-04-07 17:07:38 -0700

    ✨ Add 'Opening a File' section

--- CProjects/FileHandling/main.c
+++ CProjects/FileHandling/main.c
@@ -11,7 +11,28 @@
 
 int main() {
 
+    // --------------
+    // Opening a File
+    // --------------
+    printf("--------------\n");
+    printf("Opening a File\n");
+    printf("--------------\n");
 
+    // We use the file pointer 'FILE*' to work with files.
+    FILE* fptr;  // 'fptr' is the name of the file pointer.
+
+    // We can use the 'fopen("<filename.txt>", "<mode>")' function to open a file.
+    // The modes tell C if we want to read ('r'), or write ('w') from the file.
+    fptr = fopen("TutorialFile.txt", "r");
+
+    // Tip: If the file is not available in the specified location, it will return NULL.
+    // We can use an if...else statement to determine if the file is actually open.
+    if (fptr != NULL) {
+        printf("File Open Successful");
+    }
+    else {
+        printf("File Open Unsuccessful");
+    }
 
     return 0;
 }
