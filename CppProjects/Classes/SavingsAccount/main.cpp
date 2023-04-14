b6f48042ab07b50caf2be1de49a90490264d21c9
BryceVerberne <bryceverberne@gmail.com>
2023-04-13 20:17:47 -0700

    🌱 Add SavingsAccount lab

---
+++ CppProjects/Classes/SavingsAccount/main.cpp
@@ -0,0 +1,16 @@
+/*
+*  Title:  Savings Account
+*  Desc:   This program manages a bank account with user input through a SavingsAccount class.
+*  Author: Bryce Verberne
+*  Date:   04/13/2023
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
