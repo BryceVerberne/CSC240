da358dd333bc9d398e10f480c30d301861fd28b0
BryceVerberne <bryceverberne@gmail.com>
2023-04-14 16:23:33 -0700

    🔧 Refactor Square class, complete main function & improve documentation

--- CppProjects/Classes/SquareCalculator/main.cpp
+++ CppProjects/Classes/SquareCalculator/main.cpp
@@ -10,67 +10,83 @@
 #include <iostream>
 using namespace std;
 
-// FILL IN THE CODE TO DECLARE A CLASS CALLED Square. TO DO THIS SEE
-// THE IMPLEMENTATION SECTION.
+// Square class declaration
+class Square {
+public:  // Member Functions
 
-int main()
-{
+    // setSide: Sets the length of a side for the square object
+    // Input: float value representing the length of a side
+    // Output: None
+    void setSide(float length);
+
+    // findArea: Calculates the area of the square object
+    // Input: None
+    // Output: float value representing the area of the square
+    float findArea();
+
+    // findPerimeter: Calculates the perimeter of the square object
+    // Input: None
+    // Output: float value representing the perimeter of the square
+    float findPerimeter();
+
+private: // Data Members
+
+    // side: float value representing the length of a side of the square object
+    float side;
+};
+
+int main() {
     Square box;	// box is defined as an object of the Square class
     float size;	// size contains the length of a side of the square
 
-    // FILL IN THE CLIENT CODE THAT WILL ASK THE USER FOR THE LENGTH OF THE
-    // SIDE OF THE SQUARE. (This is stored in size)
+    // Prompt the user to enter the length of the side of the square & store it in size
+    cout << "Enter The Square's Length: ";
+    cin >> size;
 
-    // FILL IN THE CODE THAT CALLS SetSide.
+    // Pass the value of size to our object using 'setSide()'
+    box.setSide(size);
 
-    // FILL IN THE CODE THAT WILL RETURN THE AREA FROM A CALL TO A FUNCTION
-    // AND PRINT OUT THE AREA TO THE SCREEN.
+    // Use the 'findArea()' member function to print our square's area to console
+    cout << "Area: " << box.findArea() << endl;
 
-    // FILL IN THE CODE THAT WILL RETURN THE PERIMETER FROM A CALL TO A
-    // FUNCTION AND PRINT OUT THAT VALUE TO THE SCREEN.
+    // Use the 'findPerimeter()' member function to print our square's perimeter to console
+    cout << "Perimeter: " << box.findPerimeter() << endl;
 
     return 0;
 }
 
+
 // _______________________________________________________
 //
-// Implementation section	Member function implementation
-
-//**************************************************
-//  setSide
+// Implementation section
 //
-//  task:	 This procedure takes the length of a side and
-//	         places it in the appropriate member data
-//  data in: length of a side
-//***************************************************
 
-void Square::setSide(float length)
-{
+// -------
+// setSide
+// -------
+// Desc:   Sets the length of a side for the square object
+// Input:  float value representing the length of a side
+// Output: None
+void Square::setSide(float length) {
     side = length;
 }
 
-//**************************************************
-//	findArea
-//
-//  task:	       This finds the area of a square
-//  data in:       none (uses value of data member side)
-//  data returned: area of square
-//***************************************************
-
-float Square::findArea()
-{
+// --------
+// findArea
+// --------
+// Desc:   Calculates the area of the square object
+// Input:  None
+// Output: float value representing the area of the square
+float Square::findArea() {
     return side * side;
 }
 
-//**************************************************
-//	findPerimeter
-//
-//  task:	       This finds the perimeter of a square
-//  data in:       none (uses value of data member side)
-//  data returned: perimeter of square
-//***************************************************
-
-float Square::findPerimeter()
-{
+// -------------
+// findPerimeter
+// -------------
+// Desc:   Calculates the perimeter of the square object
+// Input:  None.
+// Output: float value representing the perimeter of the square
+float Square::findPerimeter() {
     return 4 * side;
 }
