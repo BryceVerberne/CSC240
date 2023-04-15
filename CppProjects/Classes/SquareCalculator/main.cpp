6bd424efc939c9279645c3f122efca5443516e0a
BryceVerberne <bryceverberne@gmail.com>
2023-04-14 17:58:46 -0700

    :construction: Add constructors & destructor to 'Square' class

--- CppProjects/Classes/SquareCalculator/main.cpp
+++ CppProjects/Classes/SquareCalculator/main.cpp
@@ -12,7 +12,20 @@ using namespace std;
 
 // Square class declaration
 class Square {
-public:  // Member Functions
+public: // Constructors & Member Functions
+
+    // Default constructor
+    // Desc: Initializes a square object with default side length of 1
+    Square();
+
+    // Parameterized constructor
+    // Desc: Initializes a square object with specified side length
+    // Input: float value representing the length of a side
+    Square(float length);
+
+    // Destructor
+    // Desc: Cleans up resources when the square object is destroyed
+    ~Square();
 
     // setSide: Sets the length of a side for the square object
     // Input: float value representing the length of a side
@@ -36,21 +49,26 @@ private: // Data Members
 };
 
 int main() {
-    Square box;	// box is defined as an object of the Square class
-    float size;	// size contains the length of a side of the square
+    Square box;	    // box is defined as an object of the Square class
+    Square box1(9); // box1 is defined as an object of the Square class
+    float size;	    // size contains the length of a side of the square
 
     // Prompt the user to enter the length of the side of the square & store it in size
-    cout << "Enter The Square's Length: ";
+    cout << "Please input the length of the side of the square" << endl;
     cin >> size;
 
     // Pass the value of size to our object using 'setSide()'
     box.setSide(size);
 
     // Use the 'findArea()' member function to print our square's area to console
-    cout << "Area: " << box.findArea() << endl;
+    cout << "The area of the square is " << box.findArea() << endl;
 
     // Use the 'findPerimeter()' member function to print our square's perimeter to console
-    cout << "Perimeter: " << box.findPerimeter() << endl;
+    cout << "The perimeter of the square is " << box.findPerimeter() << endl << endl;
+
+    // Output the area & perimeter of box1
+    cout << "The area of box1 is " << box1.findArea() << endl;
+    cout << "The perimeter of " << box1.findPerimeter() << endl;
 
     return 0;
 }
@@ -60,6 +78,23 @@ int main() {
 //
 // Implementation section
 //
+
+// Default constructor
+// Desc: Initializes a square object with default side length of 1
+Square::Square() {
+    side = 1;
+}
+
+// Parameterized constructor
+// Desc: Initializes a square object with specified side length
+// Input: float value representing the length of a side
+Square::Square(float length) {
+    side = length;
+}
+
+// Destructor
+// Desc: Cleans up resources when the square object is destroyed
+Square::~Square() {}
 
 // -------
 // setSide
