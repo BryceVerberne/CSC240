3fe6f86b9aabf5c11c5e4953def94d514d3606fa
BryceVerberne <bryceverberne@gmail.com>
2023-04-19 12:04:04 -0700

    ✨ Add functions to create a prime array & print it to console

--- CppProjects/PrimeNumbers/main.cpp
+++ CppProjects/PrimeNumbers/main.cpp
@@ -16,52 +16,74 @@ int highBoundary();
 int* fillArray(int size);
 int nextPrime(int* numberArr, int size);
 void marker(int* numberArr, int size, int prime);
+int* arrayBuilder(int* numberArr, int size, int* newSize);
+void primePrinter(int* primeArr, int size, int bound);
 
 int main() {
-    int highBound;    // The high boundary value
-    int lowBound;     // The low boundary value
-    int size;         // The size of the number array
-    int primeNumber;  // The next prime number found in the number array
+    int highBound;      // The high boundary value
+    int lowBound;       // The low boundary value
+    int numSize;        // The size of the number array
+    int currentPrime;   // The next prime number found in the number array
+    int *numbers;       // Pointer to the dynamically-allocated number array
+    int *primeNumbers;  // Pointer to the dynamically-allocated prime number array
+    int primeSize = 0;  // The size of the prime number array
+    char input = 'y';   // User input to decide whether to continue the program or exit
+    int *sizePtr = &primeSize;  // Pointer to the primeSize variable (used as a reference parameter)
 
     // Print an introduction for the user
-    cout << "Specify a range of positive integers with a low & high boundary." << endl;
+    cout << "Hello! I'm your personal prime number checker." << endl;
 
-    // Get the low & high boundaries from the user
-    lowBound = lowBoundary();
-    highBound = highBoundary();
+    // Continue the program as long as the user inputs 'y'
+    while (input != 'n') {
+        cout << endl;
 
-    // Ensure that the low boundary is less than or equal to the high boundary
-    while (lowBound > highBound) {
-        cout << "The lower boundary must be less than or equal to the higher boundary." << endl;
-        cout << "Please enter a new set of values." << endl;
-
-        // Get new low & high boundaries from the user
+        // Get the low and high boundaries from the user
+        cout << "Provide a low and high boundary for where I can look for prime numbers." << endl;
         lowBound = lowBoundary();
         highBound = highBoundary();
-    }
 
-    // Calculate the size of the number array based on the high boundary
-    size = highBound + 1;
+        // Ensure that the low boundary is less than or equal to the high boundary
+        while (lowBound > highBound) {
+            cout << "The lower boundary must be less than or equal to the higher boundary." << endl;
+            cout << "Please enter a new set of values." << endl;
 
-    int *numbers = fillArray(size);  // Create & initialize the number array
-
-    // Find the first prime number in the number array
-    primeNumber = nextPrime(numbers, size);
-
-    // Loop until no more prime numbers are found in the number array
-    while (primeNumber != -1) {
-        // Mark the multiples of the current prime number in the number array
-        marker(numbers, size, primeNumber);
-
-        // Find the next prime number in the number array
-        primeNumber = nextPrime(numbers, size);
-
-        // Print the current state of the number array
-        for (int i = 0; i < size; ++i) {
-            cout << "Element " << i << ": " << numbers[i] << endl;
+            // Get new low and high boundaries from the user
+            lowBound = lowBoundary();
+            highBound = highBoundary();
         }
 
-        cout << endl;
+        // Assign 'numSize' to the value of highBound + 1 and initialize the 'numbers' array to this value
+        numSize = highBound + 1;
+        numbers = fillArray(numSize);
+
+        // Find the first prime number in the number array
+        currentPrime = nextPrime(numbers, numSize);
+
+        // Loop until no more prime numbers are found in the number array
+        while (currentPrime != -1) {
+            // Mark the multiples of the current prime number in the number array
+            marker(numbers, numSize, currentPrime);
+
+            // Find the next prime number in the number array
+            currentPrime = nextPrime(numbers, numSize);
+        }
+
+        // Build the prime number array using the 'numbers' array and the newSize reference parameter
+        primeNumbers = arrayBuilder(numbers, numSize, sizePtr);
+
+        // Print the prime numbers within the given boundaries
+        primePrinter(primeNumbers, primeSize, lowBound);
+
+        // Deallocate memory of dynamic arrays
+        delete[] numbers;
+        delete[] primeNumbers;
+
+        // Reset 'primeSize' in preparation for another iteration
+        primeSize = 0;
+
+        // Prompt the user to decide whether to continue the program or exit
+        cout << "Would you like to go again? [y,n]" << endl;
+        cin >> input;
     }
 
     return 0;
@@ -74,7 +96,7 @@ int main() {
  * -----------
  * Desc:   Gets the low boundary value from the user
  * Input:  None
- * Output: int value representing the low boundary
+ * Output: int - A value representing the low boundary
  * Notes:  Ensures that the low boundary value is positive
  */
 int lowBoundary() {
@@ -84,8 +106,8 @@ int lowBoundary() {
     cout << "Low Boundary: ";
     cin >> low;
 
-    // Check to see if the entered number is positive, & if not, ask the user to reenter the value
-    while (low <= 0) {
+    // Check to see if the entered number is positive, and if not, ask the user to reenter the value
+    while (low < 0) {
         cout << "Please enter a positive value: ";
         cin >> low;
     }
@@ -99,7 +121,7 @@ int lowBoundary() {
  * ------------
  * Desc:   Gets the high boundary value from the user
  * Input:  None
- * Output: int value representing the high boundary
+ * Output: int - A value representing the high boundary
  * Notes:  Ensures that the high boundary value is positive
  */
 int highBoundary() {
@@ -109,8 +131,8 @@ int highBoundary() {
     cout << "High Boundary: ";
     cin >> high;
 
-    // Check to see if the entered number is positive, & if not, ask the user to reenter the value
-    while (high <= 0) {
+    // Check to see if the entered number is positive, and if not, ask the user to reenter the value
+    while (high < 0) {
         cout << "Please enter a positive value: ";
         cin >> high;
     }
@@ -125,7 +147,7 @@ int highBoundary() {
  * Desc:   Initializes an array of integers based on the given size
  * Input:  int size - The size of the array to create
  * Output: int* - A pointer to the newly created array
- * Notes:  The array will be initialized with -1 for indices 0 & 1, & 0 for all other indices
+ * Notes:  The array will be initialized with -1 for indices 0 and 1, and 0 for all other indices
  */
 int* fillArray(int size) {
     int* numberArr = new int[size];
@@ -178,7 +200,7 @@ int nextPrime(int* numberArr, int size) {
  *         int size - The size of the number array
  *         int prime - The prime number whose multiples will be marked
  * Output: None
- * Notes:  Marks the multiples of the prime number with -1, & the prime number itself with 1
+ * Notes:  Marks the multiples of the prime number with -1, and the prime number itself with 1
  */
 void marker(int* numberArr, int size, int prime) {
     numberArr[prime] = 1;  // Mark the prime number itself with a value of 1
@@ -189,4 +211,70 @@ void marker(int* numberArr, int size, int prime) {
         numberArr[index] = -1;
         index += prime;
     }
+}
+
+/*
+ * arrayBuilder
+ * ------------
+ * Desc:   Builds a new array containing the indices of 'circled' elements (value of +1) in the input array
+ * Input:  int* numberArr - The input array of integers
+ *         int size - The size of the input array
+ *         int* newSize - A pointer to an integer representing the size of the new array (output)
+ * Output: int* - A pointer to the new array containing the indices of 'circled' elements
+ * Notes:  The function returns the new array via the 'return' statement, and the new array's size via the
+ *         reference parameter 'newSize'
+ */
+int* arrayBuilder(int* numberArr, int size, int* newSize) {
+    int* primeArr; // The new array to store the indices of 'circled' elements
+    int index = 0; // The index for the new array
+
+    // Count the number of 'circled' elements (value of +1) in the input array
+    for (int i = 0; i < size; ++i) {
+        if (numberArr[i] == 1) {
+            ++*newSize;
+        }
+    }
+
+    // Allocate memory for the new array based on the value of *newSize
+    primeArr = new int[*newSize];
+
+    // Populate the new array with the indices of 'circled' elements in the input array
+    for (int i = 0; i < size; ++i) {
+        if (numberArr[i] == 1) {
+            primeArr[index] = i;
+            ++index;
+        }
+    }
+
+    // Return the new array
+    return primeArr;
+}
+
+/*
+ * primePrinter
+ * -------------
+ * Desc:   Prints the prime numbers from the input array that are greater or equal to the given bound
+ * Input:  int* primeArr - The input array containing prime numbers
+ *         int size - The size of the input array
+ *         int bound - The lower bound for the prime numbers to print
+ * Output: None
+ * Notes:  The prime numbers are printed separated by commas
+ */
+void primePrinter(int* primeArr, int size, int bound) {
+    cout << "Prime numbers found: ";
+
+    // Iterate through the input array and print the prime numbers greater or equal to the bound
+    for (int i = 0; i < size; ++i) {
+        if (primeArr[i] >= bound) {
+            cout << primeArr[i];
+
+            // Add a comma and space if there are more prime numbers to print
+            if ((i + 1) < size) {
+                cout << ", ";
+            }
+        }
+    }
+
+    // Print a newline character to separate the output
+    cout << endl << endl;
 }
