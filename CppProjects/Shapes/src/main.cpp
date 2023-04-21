7f47f66d2f7707082502adc15a8a78ef2a790958
BryceVerberne <bryceverberne@gmail.com>
2023-04-20 18:04:19 -0700

    📝 Add TODO sections for clarity

--- CppProjects/Shapes/src/main.cpp
+++ CppProjects/Shapes/src/main.cpp
@@ -13,10 +13,24 @@
 #include "Cylinder.h"
 
 int main() {
+    // Create an Array of Shapes
+
+    // Create an instance of sphere, cube and cylinder
     Sphere ball(10.0);
     Cube cuboid(10.0);
     Cylinder tube(2.0, 5.0);
 
+    // TODO: Create variables to hold radius and height
+
+    // TODO: Ask the user for the radius of the sphere and then create an instance of that sphere
+
+    // TODO: Ask the user for the width of the cube and then create an instance of that sphere
+
+    // TODO: Ask the user for the radius and height of the cylinder
+
+    // TODO: Create an instance of the cylinder
+
+    // TODO: Print each object
     std::cout << "Sphere" << std::endl;
     std::cout << "Surface Area: " << ball.surfaceArea() << std::endl;
     std::cout << "Volume: " << ball.volume() << std::endl;
@@ -33,21 +47,13 @@ int main() {
     std::cout << "Surface Area: " << tube.surfaceArea() << std::endl;
     std::cout << "Volume: " << tube.volume() << std::endl;
 
+    // TODO: Add each object to your list
+
+
+    // TODO: Use a for loop
+    //       Print the  name of the class
+    //       Print the Surface Area
+    //       Print the volume
+
     return 0;
 }
-
-/**
-* Example Output
-* Sphere{radius=10.0}
-* Cube{width=10.0}
-* Cylinder{radius=2.0, height=5.0} *
-* Sphere
-* Surface Area: 1256.6370614359173
-* Volume: 4188.790204786391 *
-* Cube
-* Surface Area: 600.0
-* Volume: 1000.0 *
-* Cylinder
-* Surface Area: 87.96459430051421
-* Volume: 62.83185307179586
-*/
