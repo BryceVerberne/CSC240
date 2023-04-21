3121f490a77a29c6af550b14eb375927406fe4a4
BryceVerberne <bryceverberne@gmail.com>
2023-04-20 20:19:38 -0700

    ✨ Add getParameter function

--- CppProjects/Shapes/src/main.cpp
+++ CppProjects/Shapes/src/main.cpp
@@ -8,34 +8,51 @@
 
 
 #include <iostream>
+#include "Shape.h"
 #include "Sphere.h"
 #include "Cube.h"
 #include "Cylinder.h"
 
+// Prototypes
+double getParameter();
+
 int main() {
-    // Create an Array of Shapes
-
-    // Create an instance of sphere, cube and cylinder
-    Sphere ball(10.0);
-    Cube cuboid(10.0);
-    Cylinder tube(2.0, 5.0);
-
-    // TODO: Create variables to hold radius and height
+    Shape* shapes[6];         // Create an Array of Shapes
+    Sphere ball1(10.0);        // Create an instance of Sphere
+    Cube cuboid1(10.0);        // Create an instance of Cube
+    Cylinder tube1(2.0, 5.0);  // Create an instance of Cylinder
+    double radius;
+    double width;
+    double height;
 
     // TODO: Ask the user for the radius of the sphere and then create an instance of that sphere
+    std::cout << "Enter the sphere's radius: ";
+    radius = getParameter();
+    std::cout << std::endl;
 
-    // TODO: Ask the user for the width of the cube and then create an instance of that sphere
+    Sphere ball2(radius);
 
-    // TODO: Ask the user for the radius and height of the cylinder
+    // TODO: Ask the user for the width of the cube and then create an instance of that cube
+    std::cout << "Enter the cube's width: ";
+    width = getParameter();
+    std::cout << std::endl;
 
-    // TODO: Create an instance of the cylinder
+    Cube cuboid2(width);
+
+    // TODO: Ask the user for the radius and height of the cylinder and then create an instance of the cylinder
+    std::cout << "Enter the cylinder's radius: ";
+    radius = getParameter();
+
+    std::cout << "Enter the cylinder's height: ";
+    height = getParameter();
+    std::cout << std::endl;
+
+    Cylinder tube2(radius, height);
 
     // TODO: Print each object
-    std::cout << ball.toString() << std::endl << std::endl;
-
-    std::cout << cuboid.toString() << std::endl << std::endl;
-
-    std::cout << tube.toString() << std::endl;
+    std::cout << ball1.toString() << std::endl << std::endl;
+    std::cout << cuboid1.toString() << std::endl << std::endl;
+    std::cout << tube1.toString() << std::endl;
 
     // TODO: Add each object to your list
 
@@ -46,4 +63,19 @@ int main() {
     //       Print the volume
 
     return 0;
+}
+
+// TODO: Write a method called getParameter that returns a positive integer
+//       It must continually ask the user to enter the value until a valid number is entered
+//       It must handle invalid values (Exception Handling)
+double getParameter() {
+    double r;
+
+    std::cin >> r;
+    while(r < 0) {
+        std::cout << "Please enter a positive value: ";
+        std::cin >> r;
+    }
+
+    return r;
 }
